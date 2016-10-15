573dab2a5e90e053860512294bb55a7c2d097e37
Andrew Woodward <andrewmw@uvic.ca>
2016-10-14 19:42:16 -0700

    mathagram working for challenge inputs

--- Mathagrams/Mathagram.py
+++ Mathagrams/Mathagram.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 def create_list(input):
@@ -9,7 +10,6 @@ def create_list(input):
 	
 	for letter in input:
 		if not (letter == " " or letter == "+" or letter == "="):
-			print(letter)
 			return_list.append(letter)
 			if(letter == "x"):
 				num_x += 1
@@ -20,29 +20,64 @@ def create_list(input):
 	
 	
 
+
+def list_check(input_list):
+	
+	first_val = int(input_list[0] + input_list[1] + input_list[2])
+	second_val = int(input_list[3] + input_list[4] + input_list[5])
+	sum = int(input_list[6] + input_list[7] + input_list[8])
+	
+	ret_string = str(first_val) + " + " + str(second_val) + " = " + str(sum)
+	if first_val + second_val == sum:
+		return True, ret_string
+	return False, ret_string
+	
+
+	
+
+
 def mathagram_solve(input):
 	
 	item_list, num_x, remaining = create_list(input)
-	print(item_list)
-	print(num_x)
-	print(remaining)
 	
-	for i in range(factorial(num_x)):
+	new_list = item_list[:]
+	
+	#print("item_list: ", item_list)
+	#print("new_list: ", new_list)
+	#print("remaining: ", remaining)
+	#print("")
+	
+	for x in range(100000):
+		random.shuffle(remaining)
+		i = 0
+		j = 0
+		for item in item_list:
+			if item == 'x':
+				new_list[i] = str(remaining[j])
+				j+=1
+			i+=1
 		
-
+		#print("item_list: ", item_list)
+		#print("new_list: ", new_list)
+		#print("remaining: ", remaining)
+		#print("")
+ 		
+		bool_check,output_string = list_check(new_list)
+ 		
+		if bool_check:
+			return output_string
+	
+	return "No solution found"
+	
 	
 
-		
 
 
 
-
-
-
-
-
-
-mathagram_solve("1xx + xxx = 468")
+print(mathagram_solve("1xx + xxx = 468"))
+print(mathagram_solve("xxx + x81 = 9x4"))
+print(mathagram_solve("xxx + 5x1 = 86x"))
+print(mathagram_solve("xxx + 39x = x75"))
 
 
 
